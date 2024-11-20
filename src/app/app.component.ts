1b5b72674e48bebfd17a20dcd3687e688e614182
Philip Szalla <philip@szalla.de>
2024-11-20 19:51:39 +0000

    feat: add year selection

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,41 @@
 import { Component, inject } from '@angular/core';
 import { FormBuilder, FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
 
-const BEITRAGSBEMESSUNGSGRENZE_KV = 5175;
-const BEITRAGSSATZ_KV = 14.6;
-const BEITRAGSSATZ_PV = 3.4;
-const BEITRAGSSATZ_PV_NO_CHILD = 0.6;
+interface IKonstanten {
+  BEITRAGSBEMESSUNGSGRENZE_KV_PV: number;
+  BEITRAGSSATZ_KV: number;
+  BEITRAGSSATZ_PV: number;
+  BEITRAGSSATZ_PV_NO_CHILD: number;
 
-const BEITRAGSBEMESSUNGSGRENZE_RV = 7550;
-const BEITRAGSSATZ_RV = 18.6;
-const BEITRAGSSATZ_AV = 2.6;
+  BEITRAGSBEMESSUNGSGRENZE_RV_AV: number;
+  BEITRAGSSATZ_RV: number;
+  BEITRAGSSATZ_AV: number;
+}
+
+const jahre = ['2024', '2025'] as const;
+
+type year = typeof jahre[number];
+
+const konstanten: { [key in year]: IKonstanten } = {
+  '2024': {
+    BEITRAGSBEMESSUNGSGRENZE_KV_PV: 5175,
+    BEITRAGSSATZ_KV: 14.6,
+    BEITRAGSSATZ_PV: 3.4,
+    BEITRAGSSATZ_PV_NO_CHILD: 0.6,
+    BEITRAGSBEMESSUNGSGRENZE_RV_AV: 7550,
+    BEITRAGSSATZ_RV: 18.6,
+    BEITRAGSSATZ_AV: 2.6,
+  },
+  '2025': {
+    BEITRAGSBEMESSUNGSGRENZE_KV_PV: 5512.5,
+    BEITRAGSSATZ_KV: 14.6,
+    BEITRAGSSATZ_PV: 3.4,
+    BEITRAGSSATZ_PV_NO_CHILD: 0.6,
+    BEITRAGSBEMESSUNGSGRENZE_RV_AV: 8050,
+    BEITRAGSSATZ_RV: 18.6,
+    BEITRAGSSATZ_AV: 2.6,
+  }
+}
 
 @Component({
   selector: 'app-root',
@@ -19,11 +46,13 @@ const BEITRAGSSATZ_AV = 2.6;
 export class AppComponent {
   private fb = inject(FormBuilder);
 
+  jahre = jahre;
   data: IEinkommenData[] = [];
   private counter = 0;
 
   fg = this.fb.group<EinkommenDataForm>({
     id: new FormControl(0, { nonNullable: true }),
+    jahr: new FormControl(jahre[0], { nonNullable: true }),
     bruttoMonat: new FormControl(0, { nonNullable: true, validators: [Validators.required, Validators.min(0)] }),
     age: new FormControl(28, { nonNullable: true, validators: [Validators.required, Validators.min(16)] }),
     zusatzBeitragKv: new FormControl(1.7, { nonNullable: true, validators: [Validators.required, Validators.min(0)] }),
@@ -48,39 +77,39 @@ export class AppComponent {
     return Math.round(num * 100) / 100;
   }
 
-  private getKvBrutto(brutto: number) {
-    return Math.min(brutto, BEITRAGSBEMESSUNGSGRENZE_KV)
+  private getKvPvBrutto(item: IEinkommenData) {
+    return Math.min(item.bruttoMonat, konstanten[item.jahr].BEITRAGSBEMESSUNGSGRENZE_KV_PV)
   }
 
   getKv(item: IEinkommenData) {
-    const factor = (BEITRAGSSATZ_KV + item.zusatzBeitragKv) / 100 / 2
+    const factor = (konstanten[item.jahr].BEITRAGSSATZ_KV + item.zusatzBeitragKv) / 100 / 2
 
-    return this.toFixed(this.getKvBrutto(item.bruttoMonat) * factor);
+    return this.toFixed(this.getKvPvBrutto(item) * factor);
   }
 
   getPv(item: IEinkommenData, arbeitnehmer: boolean) {
-    let factor = BEITRAGSSATZ_PV / 100 / 2;
+    let factor = konstanten[item.jahr].BEITRAGSSATZ_PV / 100 / 2;
     if (arbeitnehmer && item.age >= 23 && !item.hasChildren) {
-      factor += BEITRAGSSATZ_PV_NO_CHILD / 100;
+      factor += konstanten[item.jahr].BEITRAGSSATZ_PV_NO_CHILD / 100;
     }
 
-    return this.toFixed(this.getKvBrutto(item.bruttoMonat) * factor);
+    return this.toFixed(this.getKvPvBrutto(item) * factor);
   }
 
-  private getRvBrutto(brutto: number) {
-    return Math.min(brutto, BEITRAGSBEMESSUNGSGRENZE_RV)
+  private getRvAvBrutto(item: IEinkommenData) {
+    return Math.min(item.bruttoMonat, konstanten[item.jahr].BEITRAGSBEMESSUNGSGRENZE_RV_AV)
   }
 
   getRv(item: IEinkommenData) {
-    const factor = BEITRAGSSATZ_RV / 100 / 2
+    const factor = konstanten[item.jahr].BEITRAGSSATZ_RV / 100 / 2
 
-    return this.toFixed(this.getRvBrutto(item.bruttoMonat) * factor);
+    return this.toFixed(this.getRvAvBrutto(item) * factor);
   }
 
   getAv(item: IEinkommenData) {
-    const factor = BEITRAGSSATZ_AV / 100 / 2
+    const factor = konstanten[item.jahr].BEITRAGSSATZ_AV / 100 / 2
 
-    return this.toFixed(this.getRvBrutto(item.bruttoMonat) * factor);
+    return this.toFixed(this.getRvAvBrutto(item) * factor);
   }
 }
 
@@ -90,6 +119,7 @@ type EinkommenDataForm = {
 
 interface IEinkommenData {
   id: number;
+  jahr: year;
   bruttoMonat: number;
   age: number;
   zusatzBeitragKv: number;
